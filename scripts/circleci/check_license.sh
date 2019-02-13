34671bf7510689df6ec85d75cbb4f684e214ab58
Andrew Coates (REDMOND) <acoates@microsoft.com>
2019-02-13 14:52:49 -0800

    more change

--- scripts/circleci/check_license.sh
+++ scripts/circleci/check_license.sh
@@ -3,11 +3,15 @@
 set -e
 
 # Make sure we don't introduce accidental references to PATENTS.
-EXPECTED='Folly/folly/experimental/DynamicParser-inl.h\nFolly/folly/experimental/DynamicParser.cpp\nFolly/folly/experimental/DynamicParser.h\nFolly/folly/experimental/test/DynamicParserTest.cpp\nscripts/circleci/check_license.sh'
+EXPECTED='Folly/folly/experimental/DynamicParser-inl.h
+Folly/folly/experimental/DynamicParser.cpp
+Folly/folly/experimental/DynamicParser.h
+Folly/folly/experimental/test/DynamicParserTest.cpp
+scripts/circleci/check_license.sh'
 ACTUAL=$(git grep -l PATENTS)
 
 if [ "$EXPECTED" != "$ACTUAL" ]; then
   echo "PATENTS crept into some new files?"
-  diff -u <(echo -e "$EXPECTED") <(echo "$ACTUAL") || true
+  diff -u <(echo "$EXPECTED") <(echo "$ACTUAL") || true
   exit 1
 fi
